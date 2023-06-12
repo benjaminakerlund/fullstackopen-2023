1a63e3521f88ebd0d032189c9d48b53dcad29a25
Benjamin Åkerlund <astrocoder@pop-os.localdomain>
2023-06-12 14:00:22 +0300

    Completed 1.11 and displayed statistics as a html-table. This completely voids the 1.10 method, so i kept both in the final look of the webpage for clarity that I have in fact completed both.

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,7 +11,7 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 const StatisticLine = (props) => <p>{props.text} {props.value} {props.text2}</p>
 
 const Statistics = (props) => {
-// 1.10 add if statement 
+// 1.9 add if statement 
 if (props.all === 0) {
   console.log("All is zero...")
   return (
@@ -23,7 +23,7 @@ if (props.all === 0) {
   )
 }
 
-/** 1.11 create a different StatisticLine function for displaying single statistic
+/** 1.10 create a different StatisticLine function for displaying single statistic
  * This will ruin the "positive" statistics line 
  *    since we need to forcefully somehow add a "%" sign after
  *    notice the ugly fix made when calling the Statistics component for positive...
@@ -106,6 +106,7 @@ const App = () => {
     setPos(100 * good / updatedAll)
   }  
 
+  //This app now returns both ways to display statistics for both 1.10 and 1.11
   return (
     <div>
       <h1>give feedback</h1>
@@ -113,8 +114,18 @@ const App = () => {
       <Button handleClick={handleClickGood} text="good" />
       <Button handleClick={handleClickNeutral} text="neutral" />
       <Button handleClick={handleClickBad} text="bad" />
-
       <Statistics good={good} neutral={neutral} bad={bad} all={all} ave={ave} pos={pos}/>
+
+      <h1>Testing using a HTML-table for 1.11 below:</h1>
+      <h1>statistics</h1>
+      <table>
+        <tr><tbody>good {good}</tbody></tr>
+        <tr><tbody>bad {bad}</tbody></tr>
+        <tr><tbody>all {all}</tbody></tr>
+        <tr><tbody>average {ave}</tbody></tr>
+        <tr><tbody>positive {pos} %</tbody></tr>
+
+      </table>
     </div>
   )
 } 
