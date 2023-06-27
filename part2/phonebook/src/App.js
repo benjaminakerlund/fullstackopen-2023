147991009e753bddb6a4b9e71a62eb0e0dbeb372
Benjamin Åkerlund <astrocoder@pop-os.localdomain>
2023-06-27 14:14:45 +0300

    Finished exercise 2.11. There might also be some erroneus edits done to the part0 files when testing around, however these should not be regarded in the final submission of the part0 exercises.

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import ShowAll from './components/ShowAll'
 import ShowPerson from './components/ShowPerson'
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  /*const [persons, setPersons] = useState([
     { name: 'Arto Hellas' , number: '041-1234567'},
     { name: 'Ada Lovelace', number: '044-3456997'}
-  ]) 
+  ]) */
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('add a name...')
   const [newNumber, setNewNumber] = useState('add a phonenumber...')
-  const [nameList, setNameList] = useState([persons[0].name, persons[1].name])
-  const [numberList, setNumberList] = useState([persons[0].number, persons[1].number])
+  const [nameList, setNameList] = useState([])
+  const [numberList, setNumberList] = useState([])
   const [filterName, setFilterName] = useState('')
+
+  // Fecth stored data from database
+  // 2.11
+  useEffect( () => {
+    console.log("effect")
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("promise fulfilled")
+        setPersons(response.data)
+      })
+  }, [])
+  console.log("render", persons.length, "notes")
   
 
   // Event handlers 
--- part2/phonebook/src/components/ShowAll.js
+++ part2/phonebook/src/components/ShowAll.js
@@ -3,14 +3,16 @@ import ShowPerson from "./ShowPerson"
 
 
 const ShowAll = (props) => {
-    return (
-    <div>
-        {props.persons.map(element => <ShowPerson 
-        key={element.name} 
-        name={element.name} 
-        number={element.number} /> )} 
-    </div>
-    )
+    if (props.persons.length >= 1) {
+        return (
+        <div>
+            {props.persons.map(element => <ShowPerson 
+            key={element.name} 
+            name={element.name} 
+            number={element.number} /> )} 
+        </div>
+        )
+    }
 }
 
 export default ShowAll
