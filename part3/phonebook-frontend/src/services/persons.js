65f1c5c2553c076d99c88d629d8472a89a3b8583
benjaminakerlund <benjamin.akerlund@hotmail.com>
2023-07-07 10:35:31 +0300

    updated adress for baseUrl

--- part3/phonebook-frontend/src/services/persons.js
+++ part3/phonebook-frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://phonebook-backend-xec1.onrender.com/api/persons'
+const baseUrl = 'api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
