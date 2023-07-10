1451ac6ef92126607067e6714174c87e7fd0f099
Benjamin Åkerlund <astrocoder@pop-os.localdomain>
2023-07-10 09:16:25 +0300

    Addingoriginal backend and frontend of phonebook part3 here

---
+++ part4/phonebook/backend/mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length<3) {
+	console.log("give password as argument")
+	process.exit(1)
+}
+
+const password = process.argv[2]
+const newName = process.argv[3]
+const newNumber = process.argv[4]
+const appName = "PhonebookDB"
+
+const url =
+  `mongodb+srv://benjaminakerlund:${password}@cluster0.qrvfmu5.mongodb.net/${appName}?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery",false)
+mongoose.connect(url)
+
+
+/** Create entry schema for singular contacts */
+const contactSchema = new mongoose.Schema({
+	name: String,
+	number: String,
+})
+const Contact = mongoose.model("Contact", contactSchema)
+
+
+if (newName && newNumber) {
+	/** Add a new document (contact) to the DB */
+	const contact = new Contact({
+		name: newName,
+		number: newNumber,
+	})
+
+	contact.save().then(() => {
+		console.log(`added ${newName} number ${newNumber} to phonebook`)
+		mongoose.connection.close()
+	})
+} else { //print out phonebook contents
+	Contact.find({}).then(result => {
+		result.forEach(contact => {
+			console.log(contact)
+		})
+		mongoose.connection.close()
+	})
+}
+
