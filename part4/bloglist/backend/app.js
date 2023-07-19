9e95a08a76a86a08d4218427df35c9f5fb87c458
Benjamin Åkerlund <astrocoder@pop-os.localdomain>
2023-07-19 10:47:56 +0300

    4.12, removed catch

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -1,5 +1,6 @@
 const config = require("./utils/config")
 const express = require("express")
+require("express-async-errors") // 4b, eliminating try-catch
 const app = express()
 const cors = require("cors")
 const blogsRouter = require("./controllers/blogs")
--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -31,39 +31,29 @@ blogsRouter.post("/", async (request, response) => {
     const blog = new Blog(request.body) 
 
     // 4.12*? adding error handling
-    try {    
-        if (blog.title && blog.url) {
-            const savedBlog = await blog.save() //4.10
-            response.status(201).json(savedBlog)
-        } else {
-            response.status(400).end()
-        } 
-    } catch(exception) {
-        next(exception)
-    }
+    if (blog.title && blog.url) {
+        const savedBlog = await blog.save() //4.10
+        response.status(201).json(savedBlog)
+    } else {
+        response.status(400).end()
+    } 
+
 })
 
 // This is extra???
 blogsRouter.get("/:id", async (request, response, next) => {
-    try {
-        const blog = await Blog.findById(request.params.id)
-        if (blog) {
-            response.json(blog)
-        } else {
-            response.status(404).end()
-        } 
-    } catch(exception) {
-        next(exception)
-    }
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    } 
+
 })
 
 blogsRouter.delete("/:id", async (request, response, next) => {
-    try {
-        await Blog.findByIdAndRemove(request.params.id)
-        response.status(204).end()  
-    } catch(expection) {
-        next(expection)
-    }
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()  
 })
 
 module.exports = blogsRouter
