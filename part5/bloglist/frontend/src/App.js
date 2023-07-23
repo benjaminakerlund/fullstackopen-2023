2c2b50fab0d04c1633074a2932d502c0a0f723ca
Benjamin Åkerlund <astrocoder@pop-os.localdomain>
2023-07-23 10:02:52 +0300

    5.7

--- part5/bloglist/frontend/src/App.js
+++ part5/bloglist/frontend/src/App.js
@@ -108,10 +108,14 @@ const App = () => {
                     <NewBlogForm createBlog={handleCreate} />                    
                 </Togglable>
                 
-                
+
                 {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} />
+                <Blog 
+                    key={blog.id} 
+                    blog={blog}
+                 />
                     )}
+
             </div> 
             
         )
--- part5/bloglist/frontend/src/components/Blog.js
+++ part5/bloglist/frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from "react"
+
+
+const Blog = ({blog}) => {
+    const [visible, setVisible] = useState(false)
+
+    const hideWhenVisible = { display: visible ? 'none' : '' }
+    const showWhenVisible = { display: visible ? '' : 'none' }
+    
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: "solid",
+        borderWidth: 1,
+        marginBottom: 5
+        }
+
+
+    const setVisibility = () => {
+        setVisible(!visible)
+    }
+    return(
+        <div style={blogStyle} >
+            <div style={hideWhenVisible}>
+                {blog.title} {blog.author}
+                <button onClick={setVisibility}>view </button>
+            </div>
+
+            <div style={showWhenVisible}>
+                {blog.title} {blog.author} <br />
+                {blog.url} <br />
+                {blog.likes} 
+                <button>like</button> <br />
+                {blog.user.username} <br />
+
+                <button onClick={setVisibility}>hide</button>
+
+            </div>
+            
+        </div>
+    )
+}
 
 export default Blog
